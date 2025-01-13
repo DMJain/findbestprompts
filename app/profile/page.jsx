a7b7abc28781e863c81b164bd80706ed28b2d709
Darshan Jain <darshan.j098@gmail.com>
2025-01-13 22:40:45 +0530

    profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,38 @@
+'use client'
+import {useState, useEffect} from 'react'
+import {useSession} from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Profile from '@components/Profile'
+
+const ProfilePage = () => {
+    const {data: session} = useSession();
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+          setPosts(data);
+        }
+        if(session?.user.id){
+            fetchPost();
+        };
+      }, []);
+
+    const handleEdit = () => {}
+
+    const handleDelete = async () => {}
+  return (
+    <Profile
+        name="My"
+        desc='Welcome to your profile page'
+        data={posts}
+        handeleEdit={handleEdit}
+        handleDelete={handleDelete}
+    />
+  )
+}
+
+export default ProfilePage
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,11 +2,21 @@
 import {useState} from 'react';
 import Image from 'next/image';
 import {useSession} from 'next-auth/react';
-import {usePathname, useRouter} from 'next/router';
+import {usePathname, useRouter} from 'next/navigation';
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
-
+  const {data: session} = useSession();
   const [copied, setCopied] = useState("");
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => {
+      setCopied("");
+    }, 3000);
+  };
 
   return (
     <div className='prompt_card'>
@@ -28,7 +38,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
             </p>
           </div>
         </div>
-        <div className='copy_btn' onClick={() => {}}>
+        <div className='copy_btn' onClick={handleCopy}>
           <Image 
             src={copied === post.prompt
               ? '/assets/icons/tick.svg'
@@ -47,6 +57,20 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p 
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}>
+            Edit
+          </p>
+          <p 
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
