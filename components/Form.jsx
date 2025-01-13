8311ca4dba7bd6f177c97598a23ae0c6496a5abd
Darshan Jain <darshan.j098@gmail.com>
2025-01-13 23:46:11 +0530

    addvance search

--- components/Form.jsx
+++ components/Form.jsx
@@ -50,7 +50,7 @@ const Form = ({type,
           <div className='flex-end mx-3 mb-5 gap-4'>
             <Link href='/' className='text-gray-500 text-sm'>Cancel</Link>
             <button type="submit" disabled={submitting} className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'>
-              {submitting ? `${type}...` : `${type}`}
+              {submitting ? `${type}ing...` : `${type}`}
             </button>
           </div>
       </form>
