2fc1749099196f2869cc8ec9c1993f6d9e06f91e
Darshan Jain <darshan.j098@gmail.com>
2025-01-13 23:20:46 +0530

    delet functionlity done

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -22,11 +22,25 @@ const ProfilePage = () => {
         };
       }, []);
 
-      const handleEdit = (post) => {
-        router.push(`/update-prompt?id=${post._id}`);
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id.toString()}`);
     }
 
-    const handleDelete = async (post) => {}
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm('Are you sure you want to delete this post?');
+
+      if(hasConfirmed){
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: 'DELETE',
+          });
+          const filteredPosts = posts.filter((p) => p._id !== post._id);
+          setPosts(filteredPosts);
+        } catch(error) {
+          console.log('Error deleting post', error);
+        }
+      }
+    }
   return (
     <Profile
         name="My"
